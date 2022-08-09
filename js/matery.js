45d0d100217cc303e3fcbef653e6d46b10dd79af
liaojunkun <1668235323@qq.com>
2022-08-09 23:53:50 +0800

    Site updated: 2022-08-09 23:53:49

--- js/matery.js
+++ js/matery.js
@@ -106,10 +106,6 @@ $(function () {
             subHtmlSelectorRelative: true
         });
 
-        $(document).find('img[data-original]').each(function(){
-            $(this).parent().attr("href", $(this).attr("data-original"));
-    });
-
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
